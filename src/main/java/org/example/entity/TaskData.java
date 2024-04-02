b11d678b22052f3b906a1a41daba27ce32dabb44
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-04-02 16:18:30 +0300

    Create TaskData Class Getter&Setters, toString Method

--- src/main/java/org/example/entity/TaskData.java
+++ src/main/java/org/example/entity/TaskData.java
@@ -16,4 +16,46 @@ public class TaskData {
         this.carolsTasks = carolsTasks;
         this.unassignedTasks = unassignedTasks;
     }
+
+    public Set<Task> getAnnsTasks() {
+        return annsTasks;
+    }
+
+    public void setAnnsTasks(Set<Task> annsTasks) {
+        this.annsTasks = annsTasks;
+    }
+
+    public Set<Task> getBobsTasks() {
+        return bobsTasks;
+    }
+
+    public void setBobsTasks(Set<Task> bobsTasks) {
+        this.bobsTasks = bobsTasks;
+    }
+
+    public Set<Task> getCarolsTasks() {
+        return carolsTasks;
+    }
+
+    public void setCarolsTasks(Set<Task> carolsTasks) {
+        this.carolsTasks = carolsTasks;
+    }
+
+    public Set<Task> getUnassignedTasks() {
+        return unassignedTasks;
+    }
+
+    public void setUnassignedTasks(Set<Task> unassignedTasks) {
+        this.unassignedTasks = unassignedTasks;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskData{" +
+                "annsTasks=" + annsTasks +
+                ", bobsTasks=" + bobsTasks +
+                ", carolsTasks=" + carolsTasks +
+                ", unassignedTasks=" + unassignedTasks +
+                '}';
+    }
 }
